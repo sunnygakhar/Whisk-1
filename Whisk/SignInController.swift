19370e3276ad3b50d88b8f07fabde6d879a58c45
Sunny Gakhar <sgakhar@andrew.cmu.edu>
2015-09-20 01:55:59 -0400

    get ques from server and location to go to from server

--- Whisk/SignInController.swift
+++ Whisk/SignInController.swift
@@ -14,6 +14,15 @@ class SignInController: UIViewController {
     
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     
+    func notInTable(email: String) -> Bool{
+        for userEmail in self.appDelegate.defaults.users{
+        if (userEmail == email){
+            return false
+            }
+        }
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         appDelegate.oauth2.onAuthorize = { parameters in
@@ -31,7 +40,6 @@ class SignInController: UIViewController {
                             data!,
                             options: NSJSONReadingOptions.AllowFragments
                             ) as! NSDictionary
-                        
                             let requrl = NSURL(string: "https://api.uber.com/v1/me")
                             let req = self.appDelegate.oauth2.request(forURL: requrl!)
                             let session = NSURLSession.sharedSession()
@@ -45,22 +53,52 @@ class SignInController: UIViewController {
                                             data!,
                                             options: NSJSONReadingOptions.AllowFragments
                                             ) as! NSDictionary
-                                        
+                                        //self.appDelegate.
+                                        if (notInTable(result[0])){
+                                            let questionsURL = NSURL(string: "<server>/whisk/questions")
+                                            let qreq = self.appDelegate.oauth2.request(forURL: questionsURL!)
+                                            let qsession = NSURLSession.sharedSession()
+                                            let task = qsession.dataTaskWithRequest(qreq){ data, questions, error in
+                                            DisplayQuestionsView(questions)
+                                            //add result.email to database
+                                            NSUserDefaults.append(result[0])
+                                            } else {
+                                            }
+                                
+                                        }
                                         
                                     }
                                     catch {
                                     }
                                 }
                             }
+                        
                             task.resume()
-
+                            //get the details of location to call uber
+                            let locationURL = NSURL(string: "<server>/whisk/whisk")
+                            let locationReq = self.appDelegate.oauth2.request(forURL: locationURL!)
+                            let locationSession = NSURLSession.sharedSession()
+                            let location = locationSession.dataTaskWithRequest(locationReq){ data, location, error in
+                                if (error != nil){
+                                } else {
+                                    let uberURL = NSURL(string: "https://sandbox-api.uber.com/<version>")
+                                    let uberReq = self.appDelegate.oauth2.request(forURL: uberURL?)
+                                    let qsession = NSURLSession.sharedSession()
+                                    let task = qsession.dataTaskWithRequest(uberURL){ data, questions, error in
+                                        DisplayQuestionsView(questions)
+                                    }
+                                }
+                            }
+                        
                     }
                     catch {
                     }
                 }
+                
             }
             task.resume()
         }
+        //print(result)
         appDelegate.oauth2.onFailure = { error in        // `error` is nil on cancel
             if nil != error {
                 print("Authorization went wrong: \(error!.localizedDescription)")
